22c090bc419c642a28d999b5057d2ffebd1caf7e
a4anand <anand.7719@gmail.com>
2021-05-08 17:48:24 -0400

    Create FirstProgramWithMethod.java

---
+++ FirstProgramWithMethod.java
@@ -0,0 +1,19 @@
+public class FirstProgramWithMethod {
+  public static void main(String bananas[]) {
+
+    //instructions to the compiler ===== Statements
+
+  } //Closing main
+  public void myFuntion1() {
+
+  }
+  public void myFuntion2() {
+
+  }
+  public void myFuntion4() {
+
+  }
+  public void myFuntion6() {
+
+  }
+} //Closing Class
